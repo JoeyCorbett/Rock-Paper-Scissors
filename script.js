a7e625877444b1d961cfbaeda0394cf657292c0c
JoeyCorbett <corbettjoey9@gmail.com>
2024-04-26 19:02:32 -0400

    Add more specific win/loss/tie messages and game over message
    The user will now get a more detailed win/loss/tie message which tells them what the computer had compared to them. Also shows a game over alert at the end to differentiate from the other prompts.

--- script.js
+++ script.js
@@ -41,14 +41,17 @@ function getHumanChoice() {
 
 function playRound(humanChoice, computerChoice) {
     const results = { 
-        rock: { scissors: "Win", paper: "Lose", rock: "Tie" },
-        paper: {rock: "Win", scissors: "Lose", paper: "Tie" },
-        scissors: {paper: "Win", rock: "Lose", scissors: "Tie"},
+        rock: { scissors: "You Won! Rock beats Scissors", paper: "You Lost! Paper beats Rock", rock: "You Tied! Rock ties with Rock" },
+        paper: {rock: "You Won! Paper beats Rock", scissors: "You Lost! Scissors beats Paper", paper: "You Tied! Paper ties with Paper" },
+        scissors: {paper: "You Won! Scissors beats Paper", rock: "You Lost! Rock beats Scissors", scissors: "You Tied! Scissors ties with Scissors"},
     };
 
-    message = `You ${results[humanChoice][computerChoice]}!`;
-    
-    return results[humanChoice][computerChoice] === "Win" ? humanScore++ : computerScore++;
+    message = results[humanChoice][computerChoice];
+    if (results[humanChoice][computerChoice].startsWith("You Won")) {
+        return humanScore++;
+    } else if (results[humanChoice][computerChoice].startsWith("You Lost")) {
+        return computerScore++;
+    } 
 }
 
 function playGame(){
@@ -67,11 +70,11 @@ function playGame(){
     }
     if (!exit) {
         if (humanScore > computerScore) {
-            alert("You Won!\n" + "User Score: " + humanScore + "\n" + "Computer Score: " + computerScore);
+            alert("Game Over\n\nYou Won!\n" + "User Score: " + humanScore + "\n" + "Computer Score: " + computerScore);
         } else if (humanScore < computerScore) {
-            alert("You Lost\n" + "User Score: " + humanScore + "\n" + "Computer Score: " + computerScore);
+            alert("Game Over\n\nYou Lost\n" + "User Score: " + humanScore + "\n" + "Computer Score: " + computerScore);
         } else {
-            alert("You Tied!");
+            alert("Game Over\n\nYou Tied!\n" + "User Score: " + humanScore + "\n" + "Computer Score: " + computerScore);
         }
     }
 }
